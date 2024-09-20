b8c5f427e724cb4c1fc15279f1c08419e7e0cb39
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-09-20 12:27:59 -0400

    feat:admin custom features added

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,21 @@
+from itertools import count
 from django.contrib import admin
-from .models import Collection,Product,Customer
+from django.db.models.query import QuerySet
+from django.db.models import Count
+from django.http import HttpRequest
+from .models import Collection,Product,Customer,Order
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status','collection_title']
     list_editable = ['unit_price']
     list_per_page=10
+    list_select_related=['collection']
+    list_filter=['collection','last_update']
+
+    def collection_title(self,product):
+        return product.collection.title
+        
 
     @admin.display(ordering='inventory')
     def inventory_status (self,product):
@@ -15,11 +25,31 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 class CustomerAdmin(admin.ModelAdmin):
+    #for form change fields,except,readonly ,pre populated field,
+    fields = ['first_name']
     list_display = ['first_name','last_name','membership']
     list_per_page=10
     list_editable=['membership']
+    ordering = ['first_name','last_name']
+    #startswith search name whose first letter match. to make it case insensetive we can also add i . so istartswith
+    search_fields=['first_name__istartswith','last_name__istartswith']
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display=['title','product_count']
+
+    @admin.display(ordering='product_count')
+    def product_count(self,collection):
+        return collection.product_count
+    
+    #to override base queryset
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(product_count=Count('product'))
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','customer','placed_at']
 
 # Register your models here.
-admin.site.register(Collection)
+admin.site.register(Collection,CollectionAdmin)
 # admin.site.register(Product,ProductAdmin)
 admin.site.register(Customer,CustomerAdmin)
--- store/models.py
+++ store/models.py
@@ -56,10 +56,11 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     
     def __str__(self) -> str:
-        return self.first_name
+        #combine first and last name
+        return f'{self.first_name} {self.last_name}'
     
     class Meta:
-        ordering = ['first_name']
+        ordering = ['first_name','last_name']
 
 
 class Order(models.Model):
