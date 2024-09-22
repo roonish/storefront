fc55fd86eafdb5d56136c61d33128728b146959a
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-09-22 19:10:38 -0400

    chore:View added for product in store

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('product/',views.product_list),
+    #int will validate so only integer can br sent as an id
+    path('product_detail/<int:id>/',views.product_detail)
+
+]
