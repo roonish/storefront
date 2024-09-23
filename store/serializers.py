d7b37be28fa7b237b40f8af5d8b48ad5d29b2ad4
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-09-23 18:19:32 -0400

    chore:default serializwe added, relationship shown

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
+from .models import Product,Collection
+from decimal import Decimal
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    #can also change name of serializer object by giving source
+    price = serializers.DecimalField(max_digits=6, decimal_places=2,source = 'unit_price')
+    #can also add new field which is not in db
+    tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    #to add relationship between different model
+    collection = CollectionSerializer()
+    #serializers.StringRelatedField()
+
+    def calculate_tax(self,product:Product):
+        return product.unit_price*Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
+from .models import Product,Collection
 from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view()
 def product_list(request):
-    return Response('Ok')
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(queryset,many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request,id):
