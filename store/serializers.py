625ecdb378b6c377a3d75ba2f4d55fbfd65b2753
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-09-23 18:23:37 -0400

    chore:Use of ModelSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,14 +2,20 @@ from rest_framework import serializers
 from .models import Product,Collection
 from decimal import Decimal
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
     
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields=['id','title','price','tax','collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
     #can also change name of serializer object by giving source
     price = serializers.DecimalField(max_digits=6, decimal_places=2,source = 'unit_price')
     #can also add new field which is not in db
